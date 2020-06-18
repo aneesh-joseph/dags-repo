f9c64f9a9c372db1214c3b4726f494f1570d9ace
Aneesh Joseph <admin@coderplus.com>
2020-06-18 19:23:40 +0530

    specify image

--- test-dag.py
+++ test-dag.py
@@ -4,9 +4,20 @@ from airflow.operators.bash_operator import BashOperator
 from datetime import datetime, timedelta
 
 
+executor_config = {
+    "KubernetesExecutor": {
+        "image": "apache/airflow:1.10.10.1-alpha2-python3.6",
+        "request_memory": "512Mi",
+        "request_cpu": "1000m",
+        "limit_memory": "512Mi",
+        "limit_cpu": "1000m"
+    }
+}
+
 common_task_args = {
     'retries': '1',
     'start_date': datetime(2019, 8, 26),
+    'executor_config': executor_config
 }
 
 dag = DAG('test-dag', default_args=common_task_args, is_paused_upon_creation=True,schedule_interval=None)
