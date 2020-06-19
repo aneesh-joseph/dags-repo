5f640e888b6dcebe4af53fbb8e056751f6f81a94
Aneesh Joseph <admin@coderplus.com>
2020-06-19 22:34:49 +0530

    add dag for airflow2

---
+++ test-dag-airflow2.py
@@ -0,0 +1,26 @@
+from airflow.models import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime, timedelta
+
+
+executor_config = {
+    "KubernetesExecutor": {
+        "image": "apache/airflow:1.10.10.1-alpha2-python3.6",
+    }
+}
+
+common_task_args = {
+    'retries': '1',
+    'start_date': datetime(2019, 8, 26),
+    'executor_config': executor_config
+}
+
+dag = DAG('test-dag', default_args=common_task_args, is_paused_upon_creation=True,schedule_interval=None)
+
+
+
+test = BashOperator(
+    task_id='hello_task',
+    bash_command='echo hello;sleep 60;echo done',
+    dag=dag
+)
